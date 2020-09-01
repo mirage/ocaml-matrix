f22fbc92ddb40b889ae9b5a57187a8d86163bef7
clecat <charles-edouard.lecat@epitech.eu>
2020-09-01 16:52:41 +0200

    Re-adding ocp-json-typed header mistakenly removed

--- json_encoding/json_encoding.ml
+++ json_encoding/json_encoding.ml
@@ -1,3 +1,22 @@
+(* JSON structure description using dependently typed combinators. *)
+
+(************************************************************************)
+(*  ocplib-json-typed                                                   *)
+(*                                                                      *)
+(*    Copyright 2014 OCamlPro                                           *)
+(*                                                                      *)
+(*  This file is distributed under the terms of the GNU Lesser General  *)
+(*  Public License as published by the Free Software Foundation; either *)
+(*  version 2.1 of the License, or (at your option) any later version,  *)
+(*  with the OCaml static compilation exception.                        *)
+(*                                                                      *)
+(*  ocplib-json-typed is distributed in the hope that it will be useful,*)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of      *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *)
+(*  GNU General Public License for more details.                        *)
+(*                                                                      *)
+(************************************************************************)
+
 (*
  * Heavily based on ocp-json-typed, but with slight changes to correspon to my needs
  * Should entirely reworked
