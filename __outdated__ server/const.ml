49e1201fabd831f81088ef8f3280b9fdf00a9a1d
clecat <charles-edouard.lecat@epitech.eu>
2021-12-01 16:26:19 +0100

    Remove outdated server (main server caught the important stuff)

--- __outdated__ server/const.ml
+++
@@ -1,8 +0,0 @@
-let versions = ["r0.5.0"]
-let unstable_features = None
-let homeserver = "ocaml-matrix"
-let identity_server = "vector"
-let room_version = "4"
-let upload_size = 100 * 1024 * 1024
-
-module Capabilities = struct let change_password = false end
