960210fe411afff4a0cffd3b4b3de90f004eaf6e
Lucas Pluvinage <lucas@tarides.com>
2022-01-04 18:53:47 +0100

    step 2: send messages

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,5 +1,7 @@
 let user = "bonjour"
 let pwd = "bonjour"
+let host = "localhost"
+let userid = Fmt.str "@%s:%s" user host
 
 open Shexp_process.Infix
 
@@ -17,31 +19,81 @@ let setup_server () =
        >> run "git" ["add"; "init"]
        >> run "git" ["commit"; "-m"; "init"]
        >> run "openssl" ["genpkey"; "-algorithm"; "Ed25519"; "-out"; "pkey"])
-  >> run "matrix-ci-server-setup" ["user"; "/tmp/ocaml-matrix/"; user; pwd]
-  >> spawn "matrix-ci-server-bin"
-       ["matrix.localhost"; "foo,/tmp/ocaml-matrix/pkey"]
+  >> run "matrix-ci-server-setup" ["user"; "/tmp/ocaml-matrix/"; userid; pwd]
+  >> spawn "matrix-ci-server-bin" [host; "foo,/tmp/ocaml-matrix/pkey"]
   |> map ~f:Background_command.pid
 
 module Client = Matrix_current.Raw
 
-let bench client job =
+let message =
+  Matrix_common.Events.Event_content.Message.(Text (Text.make ~body:"hello" ()))
+
+type test = {n_messages: int; max_connections: int; n_rooms: int}
+
+let v m c r = {n_messages= m; max_connections= c; n_rooms= r}
+let existing_clients = Hashtbl.create 3
+let server = Client.Server.v `Http "localhost" 8008
+
+let client max_connections =
+  try Hashtbl.find existing_clients max_connections
+  with Not_found ->
+    let client =
+      Client.v ~server
+        ~device:(Some ("dev_" ^ Int.to_string max_connections))
+        ~user ~pwd ~max_connections () in
+    Hashtbl.add existing_clients max_connections client;
+    client
+
+let all_tests =
+  [
+    v 128 1 1; v 128 4 1; v 128 16 1; v 32 1 4; v 32 4 4; v 32 16 4; v 8 1 16;
+    v 8 4 16; v 8 16 16;
+  ]
+
+let test_id {n_messages; max_connections; n_rooms} =
+  Fmt.str "%d.%d.%d" n_messages max_connections n_rooms
+
+let bench_test job ({n_messages; max_connections; n_rooms} as test) =
+  Current.Job.log job "TEST: %d/%d/%d" n_messages max_connections n_rooms;
   let open Lwt.Syntax in
   let settings =
     {Client.name= "Test room"; topic= "."; power_level_content_override= None}
   in
-  let+ result = Client.get_room ~job ~alias:"test_room" ~settings client in
-  match result with
-  | Ok v -> Printf.printf "OK: %s\n" v
-  | Error v -> Printf.printf "Error: %s\n" v
+  let client = client max_connections in
+  let* rooms_id =
+    List.init n_rooms Fun.id
+    |> Lwt_list.map_s (fun id ->
+           Client.get_room ~job
+             ~alias:(Fmt.str "test_%s_room_%d" (test_id test) id)
+             ~settings client
+           |> Lwt.map Result.get_ok) in
+  let t0 = Mtime_clock.counter () in
+  let+ () =
+    rooms_id
+    |> Lwt_list.iter_p (fun room_id ->
+           List.init n_messages ignore
+           |> Lwt_list.iter_p (fun () ->
+                  Client.post ~job ~room_id client message
+                  |> Lwt.map Result.get_ok)) in
+  let duration = Mtime_clock.count t0 in
+  test, duration
+
+let bench () =
+  let switch = Current.Switch.create ~label:"job" () in
+  let config = Current.Config.v () in
+  let job = Current.Job.create ~switch ~label:"job" ~config () in
+  Lwt_list.map_s (bench_test job) all_tests
 
 let () =
   let server_pid = Shexp_process.eval (setup_server ()) in
   Unix.sleepf 0.2;
-  let server = Client.Server.v `Http "localhost" 8008 in
-  let client = Client.v ~server ~device:None ~user ~pwd ~max_connections:64 () in
-  let switch = Current.Switch.create ~label:"job" () in
-  let config = Current.Config.v () in
-  let job = Current.Job.create ~switch ~label:"job" ~config () in
   Fun.protect ~finally:(fun () ->
       Shexp_process.(eval (run "kill" ["-9"; Int.to_string server_pid])))
-  @@ fun () -> Lwt_main.run (bench client job)
+  @@ fun () ->
+  let durations = Lwt_main.run (bench ()) in
+  Printf.printf "For all tests: 128 messages:\n";
+  List.iter
+    (fun (test, duration) ->
+      Fmt.pr "%d rooms %d connections: %a\n" test.n_rooms test.max_connections
+        Mtime.Span.pp duration)
+    durations
